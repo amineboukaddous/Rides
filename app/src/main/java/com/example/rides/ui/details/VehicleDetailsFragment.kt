f0e5966ea12f42ae34d5e4f76ba5f4fb6fce4584
Boukaddous Mohamed Amine <Mohamed.Amine.Boukaddous@ibm.com>
2023-08-02 17:23:38 +0100

    FEAT - Usage of args

--- app/src/main/java/com/example/rides/ui/details/VehicleDetailsFragment.kt
+++ app/src/main/java/com/example/rides/ui/details/VehicleDetailsFragment.kt
@@ -6,33 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.rides.R
 import com.example.rides.data.Vehicle
 import com.example.rides.databinding.FragmentVehicleDetailsBinding
 import com.example.rides.widget.VehicleDetailsBottomSheetFragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class VehicleDetailsFragment : Fragment() {
-    private lateinit var vehicle: Vehicle
+class VehicleDetailsFragment @Inject constructor(
 
+) : Fragment() {
     private var _binding: FragmentVehicleDetailsBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<VehicleDetailsViewModel>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            vehicle = it.getParcelable("vehicleArg")!!
-        }
-    }
+    private val args: VehicleDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentVehicleDetailsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
 
         return binding.root
     }
@@ -40,19 +38,23 @@ class VehicleDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.vehicleMakeModel.text = vehicle.makeModel
-        binding.vehicleVin.text = getString(R.string.vin_vehicle_detail, vehicle.vin)
-        binding.vehicleColor.text = getString(R.string.color_vehicle_detail, vehicle.color)
-        binding.vehicleType.text = getString(R.string.type_vehicle_detail, vehicle.carType)
-        viewModel.kilometrage = vehicle.kilometrage
+        binding.vehicleMakeModel.text = args.vehicle.makeModel
+        binding.vehicleVin.text = getString(R.string.vin_vehicle_detail, args.vehicle.vin)
+        binding.vehicleColor.text = getString(R.string.color_vehicle_detail, args.vehicle.color)
+        binding.vehicleType.text = getString(R.string.type_vehicle_detail, args.vehicle.carType)
+        viewModel.kilometrage = args.vehicle.kilometrage
 
-        binding.carbonEmissions.setOnClickListener{
-            val kilometrage: Int = viewModel.kilometrage
-            val emissions: String = viewModel.estimateCarboneEmissions(kilometrage).toString()
+        binding.carbonEmissions.setOnClickListener {
+            val emissions: String = viewModel.estimateCarboneEmissions(viewModel.kilometrage).toString()
             val emissionTextViewValue = getString(R.string.carbon_emission_unit, emissions)
-
             val modalBottomSheet = VehicleDetailsBottomSheetFragment(emissionTextViewValue)
-            activity?.let { it1 -> modalBottomSheet.show(it1.supportFragmentManager, VehicleDetailsBottomSheetFragment.TAG) }
+
+            activity?.let { activity ->
+                modalBottomSheet.show(
+                    activity.supportFragmentManager,
+                    VehicleDetailsBottomSheetFragment.TAG
+                )
+            }
         }
     }
 
